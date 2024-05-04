8e71b70e40c7842bd02ba5c9b089947d6474d8d0
Allin Asha I <143681192+alinasha1234@users.noreply.github.com>
2024-05-04 22:10:54 +0400

    hcf

--- hcf.java
+++ hcf.java
@@ -1,16 +1,15 @@
 public class hcf {
-  public static void main (String[]args)
-  {
-    int num1 = 36, num2 = 60, hcf=0;
-
-    for (int i = 1; i <= num1 || i <= num2; i++)
+    public static void main(String a[])
+    {
+        DataInputStream d=new DataInputStream(System.in);
+         System.out.println("enter the numbers");
+         int number1=Integer.parseInt(d.readLine());
+         int number2=Integer.parseInt(d.readLine());
+          for (int i = 1; i <= num1 || i <= num2; i++)
       {
-     if (num1 % i == 0 && num2 % i == 0)
+     if (number1 % i == 0 && number2 % i == 0)
         hcf = i;
       }
-
-    System.out.println("The HCF: "+ hcf);
-  
-
-}
+      System.out.println("The HCF: "+ hcf);
+    }
 }
